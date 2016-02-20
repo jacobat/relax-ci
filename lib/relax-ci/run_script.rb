2de789773d154cc0d892a0a2bdf496f342a34784
Jacob Atzen <jatzen@gmail.com>
2016-02-20 12:54:40 +0100

    Fix run script

--- lib/relax-ci/run_script.rb
+++ lib/relax-ci/run_script.rb
@@ -15,12 +15,14 @@ module RelaxCI
       logger.debug "Running script from #{message[:repository_path]}:#{message[:commit]}"
       repos = Rugged::Repository.new(message[:repository_path])
       commit = repos.lookup(message[:commit])
-      blob_id = commit.tree.detect{|o| o[:name] == 'bin/relax'}.fetch(:oid)
+      bin_id = commit.tree.detect{|o| o[:name] == 'bin' }.fetch(:oid)
+      bin = repos.lookup(bin_id)
+      blob_id = bin.detect{|o| o[:name] == 'relax' }.fetch(:oid)
       relax_script = repos.lookup(blob_id).content
       file = Tempfile.new('relax-ci')
       begin
         file.write(relax_script)
-        logger.debug(`cat #{file.path}`)
+        file.flush
       ensure
         file.close
         file.unlink
